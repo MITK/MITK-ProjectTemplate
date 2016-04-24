1552ddd0a91907afe2f26bd15be651b40e84e6e0
Stefan Kislinskiy <s.kislinskiy@dkfz-heidelberg.de>
2016-04-24 10:11:11 +0200

    Use src/internal convention for internal sources.

--- Plugins/my.awesomeproject.exampleplugin/files.cmake
+++ Plugins/my.awesomeproject.exampleplugin/files.cmake
@@ -1,22 +1,22 @@
 set(CPP_FILES
-  src/my_awesomeproject_exampleplugin_PluginActivator.cpp
-  src/AwesomeView.cpp
+  src/internal/my_awesomeproject_exampleplugin_PluginActivator.cpp
+  src/internal/AwesomeView.cpp
 )
 
 set(UI_FILES
-  src/AwesomeViewControls.ui
+  src/internal/AwesomeViewControls.ui
 )
 
 set(MOC_H_FILES
-  src/my_awesomeproject_exampleplugin_PluginActivator.h
-  src/AwesomeView.h
+  src/internal/my_awesomeproject_exampleplugin_PluginActivator.h
+  src/internal/AwesomeView.h
 )
 
 # List of resource files that can be used by the plugin system without loading
 # the actual plugin. For example, the icon that is typically displayed in the
 # plugin view menu at the top of the application window.
 set(CACHED_RESOURCE_FILES
-  resources/icon.xpm
+  resources/AwesomeIcon.png
   plugin.xml
 )
 
