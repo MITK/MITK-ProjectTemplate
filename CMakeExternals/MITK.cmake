d51568dc6e0afa7792e87e9efd5565739343f3fc
Stefan Kislinskiy <s.kislinskiy@dkfz-heidelberg.de>
2016-08-11 16:08:48 +0200

    Changed clone URL of MITK-ProjectTemplate

--- CMakeExternals/MITK.cmake
+++ CMakeExternals/MITK.cmake
@@ -22,22 +22,22 @@ if(NOT MITK_DIR)
   option(MITK_USE_QT "Use Nokia's Qt library in MITK" ON)
   option(MITK_USE_OpenCV "Use Intel's OpenCV library" OFF)
   option(MITK_USE_Python "Enable Python wrapping in MITK" OFF)
-  
+
   if(MITK_USE_BLUEBERRY AND NOT MITK_USE_CTK)
     message("Forcing MITK_USE_CTK to ON because of MITK_USE_BLUEBERRY")
     set(MITK_USE_CTK ON CACHE BOOL "Use CTK in MITK" FORCE)
   endif()
-  
+
   if(MITK_USE_CTK AND NOT MITK_USE_QT)
     message("Forcing MITK_USE_QT to ON because of MITK_USE_CTK")
     set(MITK_USE_QT ON CACHE BOOL "Use Nokia's Qt library in MITK" FORCE)
   endif()
-  
+
   set(MITK_USE_CableSwig ${MITK_USE_Python})
   set(MITK_USE_GDCM 1)
   set(MITK_USE_ITK 1)
   set(MITK_USE_VTK 1)
-      
+
   mark_as_advanced(MITK_USE_SUPERBUILD
                    MITK_BUILD_ALL_PLUGINS
                    MITK_BUILD_TESTING
@@ -72,7 +72,7 @@ if(NOT MITK_DIR)
       Help
 		  REQUIRED)
   endif()
-  
+
   # Configure the set of default pixel types
   set(MITK_ACCESSBYITK_INTEGRAL_PIXEL_TYPES
       "int, unsigned int, short, unsigned short, char, unsigned char"
@@ -95,11 +95,11 @@ if(NOT MITK_DIR)
     mark_as_advanced(${_arg})
     list(APPEND additional_mitk_cmakevars "-D${_arg}:STRING=${${_arg}}")
   endforeach()
-   
+
   #-----------------------------------------------------------------------------
   # Create options to inject pre-build dependencies
   #-----------------------------------------------------------------------------
-  
+
   foreach(proj CTK DCMTK GDCM VTK ITK OpenCV CableSwig)
     if(MITK_USE_${proj})
       set(MITK_${proj}_DIR "${${proj}_DIR}" CACHE PATH "Path to ${proj} build directory")
@@ -115,12 +115,12 @@ if(NOT MITK_DIR)
   if(MITK_BOOST_ROOT)
     list(APPEND additional_mitk_cmakevars "-DBOOST_ROOT:PATH=${MITK_BOOST_ROOT}")
   endif()
-  
+
   set(MITK_SOURCE_DIR "" CACHE PATH "MITK source code location. If empty, MITK will be cloned from MITK_GIT_REPOSITORY")
-  set(MITK_GIT_REPOSITORY "http://git.mitk.org/MITK.git" CACHE STRING "The git repository for cloning MITK")
+  set(MITK_GIT_REPOSITORY "https://phabricator.mitk.org/diffusion/MITK/mitk.git" CACHE STRING "The git repository for cloning MITK")
   set(MITK_GIT_TAG "releases/2016-03" CACHE STRING "The git tag/hash to be used when cloning from MITK_GIT_REPOSITORY")
   mark_as_advanced(MITK_SOURCE_DIR MITK_GIT_REPOSITORY MITK_GIT_TAG)
-  
+
   #-----------------------------------------------------------------------------
   # Create the final variable containing superbuild boolean args
   #-----------------------------------------------------------------------------
@@ -137,11 +137,11 @@ if(NOT MITK_DIR)
   if(MITK_USE_QT AND QT_QMAKE_EXECUTABLE)
     list(APPEND additional_mitk_cmakevars "-DQT_QMAKE_EXECUTABLE:FILEPATH=${QT_QMAKE_EXECUTABLE}")
   endif()
-  
+
   if(MITK_USE_CTK)
     list(APPEND additional_mitk_cmakevars "-DGIT_EXECUTABLE:FILEPATH=${GIT_EXECUTABLE}")
   endif()
-  
+
   if(MITK_INITIAL_CACHE_FILE)
     list(APPEND additional_mitk_cmakevars "-DMITK_INITIAL_CACHE_FILE:INTERNAL=${MITK_INITIAL_CACHE_FILE}")
   endif()
@@ -151,12 +151,12 @@ if(NOT MITK_DIR)
   else()
     set(MITK_BINARY_DIR ${proj}-build)
   endif()
-  
+
   set(proj_DEPENDENCIES)
   set(MITK_DEPENDS ${proj})
-  
+
   # Configure the MITK souce code location
-  
+
   if(NOT MITK_SOURCE_DIR)
     set(mitk_source_location
         SOURCE_DIR ${CMAKE_BINARY_DIR}/${proj}
@@ -188,7 +188,7 @@ if(NOT MITK_DIR)
     DEPENDS
       ${proj_DEPENDENCIES}
     )
-  
+
   if(MITK_USE_SUPERBUILD)
     set(MITK_DIR "${CMAKE_CURRENT_BINARY_DIR}/${MITK_BINARY_DIR}/MITK-build")
   else()
@@ -197,34 +197,34 @@ if(NOT MITK_DIR)
 
 else()
 
-  # The project is provided using MITK_DIR, nevertheless since other 
+  # The project is provided using MITK_DIR, nevertheless since other
   # projects may depend on MITK, let's add an 'empty' one
   MacroEmptyExternalProject(${proj} "${proj_DEPENDENCIES}")
-  
+
   # Further, do some sanity checks in the case of a pre-built MITK
   set(my_itk_dir ${ITK_DIR})
   set(my_vtk_dir ${VTK_DIR})
   set(my_qmake_executable ${QT_QMAKE_EXECUTABLE})
 
   find_package(MITK REQUIRED)
-  
+
   if(my_itk_dir AND ITK_DIR)
     if(NOT my_itk_dir STREQUAL ${ITK_DIR})
       message(FATAL_ERROR "ITK packages do not match:\n   ${MY_PROJECT_NAME}: ${my_itk_dir}\n  MITK: ${ITK_DIR}")
     endif()
   endif()
-  
+
   if(my_vtk_dir AND VTK_DIR)
     if(NOT my_vtk_dir STREQUAL ${VTK_DIR})
       message(FATAL_ERROR "VTK packages do not match:\n   ${MY_PROJECT_NAME}: ${my_vtk_dir}\n  MITK: ${VTK_DIR}")
     endif()
   endif()
-  
+
   if(my_qmake_executable AND MITK_QMAKE_EXECUTABLE)
     if(NOT my_qmake_executable STREQUAL ${MITK_QMAKE_EXECUTABLE})
       message(FATAL_ERROR "Qt qmake does not match:\n   ${MY_PROJECT_NAME}: ${my_qmake_executable}\n  MITK: ${MITK_QMAKE_EXECUTABLE}")
     endif()
   endif()
-    
+
 endif()
 
