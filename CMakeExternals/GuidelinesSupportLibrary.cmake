9d0633c360c8f3bbb425aadf1ff89294b54ad425
Stefan Dinkelacker <s.dinkelacker@dkfz-heidelberg.de>
2019-03-20 09:33:01 +0100

    Improve external project example
    - Indicate how to depend on other external projects
    - Move ${proj}_DEPENDS variable to right location

--- CMakeExternals/GuidelinesSupportLibrary.cmake
+++ CMakeExternals/GuidelinesSupportLibrary.cmake
@@ -9,9 +9,11 @@
       - CMake/PackageDepends/MITK_GuidelinesSupportLibrary_Config.cmake ]]
 
 set(proj GuidelinesSupportLibrary)
-set(${proj}_DEPENDS ${proj})
+set(proj_DEPENDENCIES "")
 
 if(MITK_USE_${proj})
+  set(${proj}_DEPENDS ${proj})
+
   if(DEFINED ${proj}_DIR AND NOT EXISTS ${${proj}_DIR})
     message(FATAL_ERROR "${proj}_DIR variable is defined but corresponds to non-existing directory!")
   endif()
@@ -25,6 +27,7 @@ if(MITK_USE_${proj})
         -DGSL_CXX_STANDARD:STRING=${MITK_CXX_STANDARD}
         -DGSL_TEST:BOOL=OFF
       CMAKE_CACHE_DEFAULT_ARGS ${ep_common_cache_default_args}
+      DEPENDS ${proj_DEPENDENCIES}
     )
 
     set(${proj}_DIR ${ep_prefix})
